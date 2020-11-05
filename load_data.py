6113a2c759b7e9021725633d6a158ae1d331e472
Alec Flowers <alec.flowers00@gmail.com>
2020-11-05 12:01:32 +0100

    FEAT: Loading Data

---
+++ load_data.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+import scipy.io
+
+def load_hrf(task = 'MOTOR', filepath = './Data/'):
+    '''
+    ['EMOTION', 'GAMBLING', 'LANGUAGE', 'MOTOR', 'RELATIONAL', 'SOCIAL', 'WM']
+    '''
+    data = scipy.io.loadmat(filepath+'X_tfMRI_'+task+'_LR_Glasser360.mat')
+    data = data['X']
+    print(f'Loaded Data - Shape: {data.shape}')
+    return data
+
+def load_task_paradigms(task = 'MOTOR', directory = './Data/TaskParadigms'):
+    FILE = task+'_LR.mat'
+    OPEN = directory + '/'
+
+    regressor = {}
+    for filename in os.listdir(directory):
+        if filename.endswith(FILE):
+            task = scipy.io.loadmat(OPEN+filename)
+            regressor[filename.split('_')[0]] = task['Regressor']
+
+    print(f'Loaded Task Paradigms - Length: {len(regressor)}')
+    return regressor
+
+if __name__ == '__main__':
+    load_hrf()
+    load_task_paradigms()
