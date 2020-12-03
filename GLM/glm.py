f67708b659976b638b6834bef9c81b4bf235416f
aglavac <a.glavackij@gmx.de>
2020-12-03 09:39:40 +0100

    REFACT: Added example for glm.

--- GLM/glm.py
+++ GLM/glm.py
@@ -1,12 +1,14 @@
 # Alec - Added this part because I wasn't apple to import. When modules are in parallel locations in folders this makes them visible to each other.
 # https://stackoverflow.com/questions/4383571/importing-files-from-different-folder
 import sys
+
 sys.path.append('../')
 
 from input_output import load
 import numpy as np
 import statsmodels.api as sm
 import scipy.io as sio
+
 
 def glm(fMRI, task_paradigms, hrf):
     """
@@ -24,7 +26,7 @@ def glm(fMRI, task_paradigms, hrf):
                     brain regions according to the result of the GLM
         betas: 2-d array of size (n_subjects, n_regions) with beta values resulting from GLM
     """
-    assert fMRI.shape[2]==task_paradigms.shape[1], \
+    assert fMRI.shape[2] == task_paradigms.shape[1], \
         f"fMRI and task_paradigms shapes do not match: {fMRI.shape[1]} and {task_paradigms.shape}"
 
     # do one hot encoding
@@ -37,8 +39,8 @@ def glm(fMRI, task_paradigms, hrf):
     print(f"Fitting GLM for {fMRI.shape[0]} subjects and {fMRI.shape[1]} regions...")
     p_value = 0.05
     activations = np.zeros((task_paradigms_one_hot.shape[0], fMRI.shape[1], task_paradigms_one_hot.shape[1] - 1))
-    betas = np.zeros((task_paradigms_one_hot.shape[0], fMRI.shape[1], task_paradigms_one_hot.shape[1] -1))
-    tvalues = np.zeros((task_paradigms_one_hot.shape[0], fMRI.shape[1], task_paradigms_one_hot.shape[1] -1))
+    betas = np.zeros((task_paradigms_one_hot.shape[0], fMRI.shape[1], task_paradigms_one_hot.shape[1] - 1))
+    tvalues = np.zeros((task_paradigms_one_hot.shape[0], fMRI.shape[1], task_paradigms_one_hot.shape[1] - 1))
 
     for subject in range(fMRI.shape[0]):
         for region in range(fMRI.shape[1]):
@@ -48,8 +50,6 @@ def glm(fMRI, task_paradigms, hrf):
             y = fMRI[subject, region, :]
             mod = sm.OLS(y, X)
             res = mod.fit()
-            if subject == 0 and region == 0:
-                print(res.summary())
             p_values = res.pvalues
             coef = res.params
             tval = res.tvalues
@@ -63,3 +63,29 @@ def glm(fMRI, task_paradigms, hrf):
     return activations, betas, tvalues
 
 
+if __name__ == "__main__":
+    tasks = [
+        'MOTOR']  # , 'GAMBLING', 'RELATIONAL', 'SOCIAL', 'WM'] # 'EMOTION', 'LANGUAGE'] # TODO: see how to fix emotion and language
+    # EMOTION is not working because fMRI has timepoints=176 and task paradigms has timepoints=186
+    # LANGUAGE is not working because each subject of the task paradigm has different length of timeseries
+    hrf = load.load_hrf_function()
+
+    for task in tasks:
+        # loading data for a specific task
+        print(f'============================== \n {task} \n==============================')
+        print(f"Loading data for task {task}...")
+        fMRI = load.load_fmri(task)
+        task_paradigms = load.load_task_paradigms(task)
+
+        # computing glm for a specific task
+        print(f'Computing GLM for task {task}...')
+        activations, betas, tvalues = glm(fMRI, task_paradigms, hrf)
+
+        # saving output for a specific task
+        print(f"Saving activations and beta values for task {task}...")
+        load.save_pickle(activations, 'activations', 'activation', task)
+        load.save_pickle(betas, 'betas', 'betas', task)
+        load.save_mat(betas, 'betas', 'betas', task)
+
+        avg = np.mean(betas, axis=0)
+        load.save_mat(avg, 'betas', 'avg_betas', task)
