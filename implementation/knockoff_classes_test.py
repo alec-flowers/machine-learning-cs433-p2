aa12f556e2e31a3579f8c764ef291f2fc04218c5
Alec Flowers <alec.flowers00@gmail.com>
2020-12-04 09:57:53 +0100

    TEST: Ran through code to make sure it works

--- implementation/knockoff_classes_test.py
+++ implementation/knockoff_classes_test.py
@@ -12,21 +12,23 @@ uncorrected_betas, corrected_betas = k.threshold(ko_betas, save=True)
 
 # Example of running GaussianKnocckOff
 
-# k_so = knockoff_class.GaussianKnockOff('MOTOR', 0)
-# k_so.load_fmri()
-# k_so.pre_process(max_corr=.3)
-# corr_g = k_so.fit()
-# # k_so.diagnostics()
-# data_so = k_so.transform()
-# ko_betas = k_so.statistic(data_so, save=True)
-# uncorrected_betas, corrected_betas = k_so.threshold(ko_betas, save=True)
+k_so = knockoff_class.GaussianKnockOff('MOTOR', 0)
+k_so.load_fmri()
+k_so.pre_process(max_corr=.3)
+corr_g = k_so.fit()
+k_so.diagnostics()
+data_so = k_so.transform()
+ko_betas = k_so.statistic(data_so, save=True)
+uncorrected_betas, corrected_betas = k_so.threshold(ko_betas, save=True)
 
 #
 # # Example of running DeepKnockOff
 #
-# d = knockoff_class.DeepKnockOff('MOTOR', 0)
-# d.pre_process(max_corr=.3)
-# # d.fit()
+d = knockoff_class.DeepKnockOff('MOTOR', 0)
+d.pre_process(max_corr=.3)
+d.fit()
 # d.load_machine()
-# # d.diagnostics()
-# data_deep = d.transform()
+d.diagnostics()
+data_deep = d.transform()
+ko_betas = d.statistic(data_deep, save=True)
+uncorrected_betas, corrected_betas = d.threshold(ko_betas, save=True)
